29eb61bf025f9d07f3a27327225cef3cc400f662
Alan Lima <alan.lima@hartbgroup.com.br>
2021-02-08 17:28:15 -0300

    Created example

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactNative = require("react-native");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var App = function App() {
+  return /*#__PURE__*/_react["default"].createElement(_reactNative.View, null, /*#__PURE__*/_react["default"].createElement(_reactNative.Text, null, "OIIIIIIII"), /*#__PURE__*/_react["default"].createElement(_reactNative.Text, null, "OIIIIIIII"), /*#__PURE__*/_react["default"].createElement(_reactNative.Text, null, "OIIIIIIII"), /*#__PURE__*/_react["default"].createElement(_reactNative.Text, null, "OIIIIIIII"));
+};
+
+var _default = App;
+exports["default"] = _default;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>OIIIIIIII</Text>
+      <Text>OIIIIIIII</Text>
+      <Text>OIIIIIIII</Text>
+      <Text>OIIIIIIII</Text>
+    </View>
+  );
+};
+
+export default App;
